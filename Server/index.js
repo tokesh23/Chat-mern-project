15957157daeec2aa5040ff96fe17f11d2bc5d480
tokesh23 <tokesh1998@gmail.com>
2024-08-22 16:28:16 +0530

    make fronted Api

---
+++ Server/index.js
@@ -0,0 +1,32 @@
+import express from "express"
+import authroutes from "./routes/auth_routes.js"
+import dotenv from "dotenv"
+import connectedTomongodb from './DB/connectmongoDb.js'
+import bodyParser from "body-parser";
+
+dotenv.config();
+
+
+const PORT = process.env.PORT
+
+
+const app = express()
+
+// app.use(express.json());// body parser-> postman req 
+
+const chat = () => {
+
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(bodyParser.json())
+
+    app.use("/api/auth", authroutes)
+
+
+    app.listen(PORT, () => {
+        connectedTomongodb();
+        console.log(`server is on PORT:${PORT}`)
+    })
+}
+
+chat()
+
